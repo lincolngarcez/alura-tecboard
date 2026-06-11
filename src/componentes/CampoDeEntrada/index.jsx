34dac2bab31262bb0e2e68812d208c226db6ec4e
Lincoln Garcez <lincoln.garcez@sefaz.am.gov.br>
2026-06-11 08:21:34 -0400

    Commit

--- src/componentes/CampoDeEntrada/index.jsx
+++ src/componentes/CampoDeEntrada/index.jsx
@@ -1,8 +1,7 @@
+import "./campo-entrada.estilos.css"
+
 //function CampoDeEntrada({type, id, placeholder, name}) {
 export function CampoDeEntrada(props) {
-  return (
-    <input
-      {...props}
-    />
-  )
+  return <input {...props}
+    className="campo-entrada-form" />
 }
