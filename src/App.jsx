ad015c470454c47ec917e6a27a0895a5f876d774
Lincoln Garcez <lincoln.garcez@sefaz.am.gov.br>
2026-06-19 08:14:02 -0400

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,85 @@
-import "./App.css";
+import "./App.css"
+import { Banner } from "./componentes/Banner"
+import { CardEvento } from "./componentes/CardEvento"
 import { FormularioDeEvento } from "./componentes/FormularioDeEvento"
+import { Tema } from "./componentes/Tema"
 
 function App() {
+
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
+  const eventos = [
+    {
+      capa: 'https://raw.githubusercontent.com/viniciosneves/tecboard-assets/refs/heads/main/imagem_1.png',
+      tema: temas[0],
+      data: new Date(),
+      titulo: 'Mulheres no Front'
+    }
+  ]
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="" />
       </header>
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
+
+      {temas.map(function(item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item}/>
+            <CardEvento evento={eventos[0]} />
+          </section>
+        )
+      })}
+
+
+      {/* <section>
+        <Tema tema={temas[0]}/>
+      </section>
+      <section>
+        <Tema tema={temas[1]}/>
+      </section>
+      <section>
+        <Tema tema={temas[2]}/>
+      </section>
+      <section>
+        <Tema tema={temas[3]}/>
+      </section>
+      <section>
+        <Tema tema={temas[4]}/>
+      </section>
+      <section>
+        <Tema tema={temas[5]}/>
+      </section> */}
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
